e42f292462d5dc35a94d88bdc738c656a38ac12a
Marcus Willock <crazcalm@gmail.com>
2021-01-10 20:17:25 -0500

    Changed the path to find static assests because, on some of my computers, setting it to ../html and html/ makes a difference

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ async fn main() {
     pretty_env_logger::init();
 
     // Serves directory for static items
-    let static_items = warp::path("static").and(warp::fs::dir("../html/static"));
+    let static_items = warp::path("static").and(warp::fs::dir("html/static"));
 
     // Main page for site
     let main_page = warp::get()
         .and(warp::path::end())
-        .and(warp::fs::file("../html/index.html"));
+        .and(warp::fs::file("html/index.html"));
 
     // Setting up my routes
     let routes = main_page.or(static_items);
